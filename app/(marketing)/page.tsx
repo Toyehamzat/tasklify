aee8e5062b94d74f807e8cb53b8263da06646f74
Toyehamzat <anibiseun@gmail.com>
2024-01-08 10:27:40 +0000

    semibold

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex items-center flex-col justify-center">
       <div className="flex items-center justify-center flex-col">
-        <div className="mb-4 flex flex-row items-center border shadow-sm p-4 rounded-full bg-amber-100 text-amber-700 uppercase ">
+        <div className="mb-4 flex flex-row items-center border shadow-sm p-4 rounded-full bg-amber-100 text-amber-700 uppercase font-semibold">
           <Medal className="h-6 w-6 mr-2" />
           No 1 task mangement
         </div>
