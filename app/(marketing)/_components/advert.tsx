5e046fb8d63b090ec829eac15ed318d3cb0bf89c
Toyehamzat <anibiseun@gmail.com>
2024-01-08 11:28:43 +0000

    added advert

---
+++ app/(marketing)/_components/advert.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Advert = () => {
+  return (
+    <div className="fixed top-14 w-full h-12 px-4 border-b shadow-sm bg-blue-100 flex items-center">
+      <div className="w-auto mx-auto flex items-center justify-between ">
+        Accelerate your teams' work with Atlassian Intelligence (AI) features 🤖
+        now in beta!{" "}
+        <span className="underline text-blue-600"> coming soon.</span>
+      </div>
+    </div>
+  );
+};
