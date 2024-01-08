7902baec8de814b4da854cbdda44d4a8df769d60
Toyehamzat <anibiseun@gmail.com>
2024-01-08 10:26:56 +0000

    medal

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
   return (
     <main className="flex items-center flex-col justify-center">
       <div className="flex items-center justify-center flex-col">
-        <div>
-          <Medal />
-          <Medal />
+        <div className="mb-4 flex flex-row items-center border shadow-sm p-4 rounded-full bg-amber-100 text-amber-700 uppercase ">
+          <Medal className="h-6 w-6 mr-2" />
+          No 1 task mangement
         </div>
         <div className=""></div>
       </div>
