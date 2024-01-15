4b5c1ed22579acd76a6495105abacf22cb1b25e5
Toyehamzat <anibiseun@gmail.com>
2024-01-15 10:47:58 +0000

    completed side bar component

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -1,33 +1,100 @@
 "use client";
 
-import React from "react";
+import Link from "next/link";
 import { Plus } from "lucide-react";
-import uselocalStorage, { useLocalStorage } from "usehooks-ts";
-import { useOrganizationList, useOrganization } from "@clerk/nextjs";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Accordion } from "@/components/ui/accordion";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
 
-type SidebarProps = {
+import { NavItem, Organization } from "./nav-item";
+
+interface SidebarProps {
   storageKey?: string;
-};
+}
 
-function Sidebar({ storageKey = "t-sidebar-state" }: SidebarProps) {
-  const [expanded, SetExpanded] = useLocalStorage<Record<string, any>>(
+export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
     storageKey,
     {}
   );
-  const { organization: activeOrganization, isLoaded: isloadedOrg } =
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
     useOrganization();
-  const { userMemberships, isLoaded: isloadedOrgList } = useOrganizationList({
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
     userMemberships: {
       infinite: true,
     },
   });
-  const 
-  return <div>Sidebar!</div>;
-}
 
-export default Sidebar;
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">Workspaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto hover:bg-slate-100"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type={"multiple"}
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
+      </Accordion>
+    </>
+  );
+};
