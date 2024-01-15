72781e7c9dddec1b5a89e026eebc32a0b0f41b47
Toyehamzat <anibiseun@gmail.com>
2024-01-15 10:47:41 +0000

    edited layout

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { SiteConfig } from "@/config/site";
-import Sidebar from "../_components/Sidebar";
+import { Sidebar } from "../_components/Sidebar";
 export const metadata: Metadata = {
   title: {
     default: SiteConfig.name,
@@ -14,13 +14,13 @@ export default function OrganizationLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="pt-20 md:pt-24 transition px-6 max-w-6xl mx-auto 2xl:max-w-screen-xl">
+    <main className="pt-20 md:pt-24 px-4  mx-auto">
       <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block">
           <Sidebar />
         </div>
         {children}
       </div>
-    </div>
+    </main>
   );
 }
