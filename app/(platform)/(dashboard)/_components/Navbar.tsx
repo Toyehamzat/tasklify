4063afecef636ce06272d6d073354f9e2be860a0
Toyehamzat <anibiseun@gmail.com>
2024-01-17 10:18:19 +0000

    added side bar component in navbar

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -4,10 +4,12 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { MobileSidebar } from "./MobileSideBar";
 
 function Navbar() {
   return (
     <nav className="fixed z-50 top-0 w-full h-14 px-4 md:px-10 border-b shadow-sm bg-white flex items-center">
+      <MobileSidebar />
       <div className="flex items-center gap-x-4 ">
         <div className="hidden md:flex">
           <Logo />
