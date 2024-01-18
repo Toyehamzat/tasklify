a3c82d467f66fa4e5e75e32f0ed16fc35f55e796
Toyehamzat <anibiseun@gmail.com>
2024-01-18 21:51:50 +0000

    updated settings page

--- app/(platform)/(dashboard)/organization/[organizationId]/settings/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/settings/page.tsx
@@ -3,18 +3,20 @@ import React from "react";
 
 function SettingsPage() {
   return (
-    <div className="w-full">
+    <div className="w-full h-full">
       <OrganizationProfile
         appearance={{
           elements: {
             rootBox: {
               boxShadow: "none",
               width: "100%",
+              height: "100%",
             },
             card: {
               border: "1px solid #e5e5e5",
               boxShadow: "none",
               width: "100%",
+              height: "100%",
             },
           },
         }}
