10325f621bebd22a8ecc7fbe2cbbfc36ddfe4316
Toyehamzat <anibiseun@gmail.com>
2024-02-08 16:12:43 +0100

    form server action

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,10 +8,10 @@ import { createSafeAction } from "@/lib/create-safe-action";
 
 import { InputType, ReturnType } from "./types";
 import { CreateBoard } from "./schema";
-import { createAuditLog } from "@/lib/create-audit-log";
-import { ACTION, ENTITY_TYPE } from "@prisma/client";
-import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
-import { checkSubscription } from "@/lib/subscription";
+// import { createAuditLog } from "@/lib/create-audit-log";
+// import { ACTION, ENTITY_TYPE } from "@prisma/client";
+// import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
+// import { checkSubscription } from "@/lib/subscription";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -22,32 +22,32 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-  const canCreate = await hasAvailableCount();
-  const isPro = await checkSubscription();
+  // const canCreate = await hasAvailableCount();
+  // const isPro = await checkSubscription();
 
-  if (!canCreate && !isPro) {
-    return {
-      error:
-        "You have reached your limit of free boards. Please upgrade to create more.",
-    };
-  }
+  // if (!canCreate && !isPro) {
+  //   return {
+  //     error:
+  //       "You have reached your limit of free boards. Please upgrade to create more.",
+  //   };
+  // }
 
-  const { title, image } = data;
+  const { title } = data;
 
-  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
-    image.split("|");
+  // const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+  //   image.split("|");
 
-  if (
-    !imageId ||
-    !imageThumbUrl ||
-    !imageFullUrl ||
-    !imageUserName ||
-    !imageLinkHTML
-  ) {
-    return {
-      error: "Missing fields. Failed to create board.",
-    };
-  }
+  // if (
+  //   !imageId ||
+  //   !imageThumbUrl ||
+  //   !imageFullUrl ||
+  //   !imageUserName ||
+  //   !imageLinkHTML
+  // ) {
+  //   return {
+  //     error: "Missing fields. Failed to create board.",
+  //   };
+  // }
 
   let board;
 
@@ -55,25 +55,25 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     board = await db.board.create({
       data: {
         title,
-        orgId,
-        imageId,
-        imageThumbUrl,
-        imageFullUrl,
-        imageUserName,
-        imageLinkHTML,
+        // orgId,
+        // imageId,
+        // imageThumbUrl,
+        // imageFullUrl,
+        // imageUserName,
+        // imageLinkHTML,
       },
     });
 
-    if (!isPro) {
-      await incrementAvailableCount();
-    }
+    // if (!isPro) {
+    //   await incrementAvailableCount();
+    // }
 
-    await createAuditLog({
-      entityTitle: board.title,
-      entityId: board.id,
-      entityType: ENTITY_TYPE.BOARD,
-      action: ACTION.CREATE,
-    });
+    // await createAuditLog({
+    //   entityTitle: board.title,
+    //   entityId: board.id,
+    //   entityType: ENTITY_TYPE.BOARD,
+    //   action: ACTION.CREATE,
+    // });
   } catch (error) {
     return {
       error: "Failed to create.",
