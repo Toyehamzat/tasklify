c2c2220dbbe0887c93ef9699c3487478b109c325
Toyehamzat <anibiseun@gmail.com>
2024-02-09 01:17:56 +0100

    added separator

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { HelpCircle, User2 } from "lucide-react";
+
+import { db } from "@/lib/db";
+// import { Hint } from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
+// import { FormPopover } from "@/components/form/form-popover";
+// import { MAX_FREE_BOARDS } from "@/constants/boards";
+// import { getAvailableCount } from "@/lib/org-limit";
+// import { checkSubscription } from "@/lib/subscription";
+
+export const BoardList = () => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="h-6 w-6 mr-2" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+          
+      </div>
+      
+    </div>
+  );
+};
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="grid gird-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+    </div>
+  );
+};
