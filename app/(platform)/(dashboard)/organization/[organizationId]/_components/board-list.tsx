00b6ff657eddfedd38d958959f9b89a04cc04771
Toyehamzat <anibiseun@gmail.com>
2024-02-13 15:53:08 +0100

    complete form popover

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -7,6 +7,7 @@ import { db } from "@/lib/db";
 // import { Hint } from "@/components/hint";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Hint } from "@/components/hint";
+import { FormPopover } from "@/components/form/form-popover";
 // import { FormPopover } from "@/components/form/form-popover";
 // import { MAX_FREE_BOARDS } from "@/constants/boards";
 // import { getAvailableCount } from "@/lib/org-limit";
@@ -20,27 +21,24 @@ export const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-      <div
+        <FormPopover sideOffset={10} side="right">
+          <div
             role="button"
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">
-             5 remaining
-            </span>npm
+            <span className="text-xs">5 remaining</span>
             <Hint
               sideOffset={40}
               description={`
                 Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.
               `}
             >
-              <HelpCircle
-                className="absolute bottom-2 right-2 h-[14px] w-[14px]"
-              />
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
             </Hint>
           </div>
+        </FormPopover>
       </div>
-      
     </div>
   );
 };
