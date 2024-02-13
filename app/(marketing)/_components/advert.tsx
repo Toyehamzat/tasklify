5261fde0dce4118310afad9204bb0bd3fb46c294
Toyehamzat <anibiseun@gmail.com>
2024-02-13 16:08:23 +0100

    added `&apos;`

--- app/(marketing)/_components/advert.tsx
+++ app/(marketing)/_components/advert.tsx
@@ -4,8 +4,8 @@ export const Advert = () => {
   return (
     <div className="fixed top-14 w-full h-12 px-4 border-b shadow-sm hidden md:flex bg-blue-100  items-center">
       <div className="w-auto mx-auto flex items-center justify-between ">
-        Accelerate your teams' work with Atlassian Intelligence (AI) features 🤖
-        now in beta!{" "}
+        Accelerate your teams`&apos;` work with Atlassian Intelligence (AI)
+        features 🤖 now in beta!{" "}
         <span className="underline text-blue-600"> coming soon.</span>
       </div>
     </div>
