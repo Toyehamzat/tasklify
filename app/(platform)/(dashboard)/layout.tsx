8097e06fb1f7bcec3902500dbc475c331ec8dc06
Toyehamzat <anibiseun@gmail.com>
2024-02-28 00:47:41 +0100

    added metadata

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
 import { SiteConfig } from "@/config/site";
 import Navbar from "./_components/Navbar";
-export const metadata: Metadata = {
-  title: {
-    default: SiteConfig.name,
-    template: `%s | ${SiteConfig.name}`,
-  },
-};
 
 export default function DashboardLayout({
   children,
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -2,20 +2,18 @@ import { Separator } from "@/components/ui/separator";
 import type { Metadata } from "next";
 import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
-
-export const metadata: Metadata = {
-  title: "dashboard",
-  description: "...",
-};
+import { Suspense } from "react";
 
 export default async function OrganizaionPage() {
   return (
     <div className="w-full mb-20">
-     <Info/>
-     <Separator className="my-4"/>
-     <div className="px-2 md:px-4">
-       <BoardList/>
-     </div>
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
     </div>
   );
 }
