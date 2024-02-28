7c864f03778dc65272934457d42e2e6c8554755e
Toyehamzat <anibiseun@gmail.com>
2024-02-28 01:25:05 +0100

    added orId type

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -4,13 +4,14 @@ export const CreateBoard = z.object({
   title: z
     .string({
       required_error: "Title is required",
-      invalid_type_error: "Title is required",
+      invalid_type_error: "Title must be a string",
     })
     .min(3, {
       message: "Title is too short.",
     }),
   image: z.string({
     required_error: "Image is required",
-    invalid_type_error: "Image is required",
+    invalid_type_error: "Image must be a string",
   }),
+  orgId: z.string(), // Define orgId property
 });
