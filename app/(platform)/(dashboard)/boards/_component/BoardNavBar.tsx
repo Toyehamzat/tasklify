d2153bd2ac5e0a8d7f776653c91b2b36397bd131
Toyehamzat <anibiseun@gmail.com>
2024-02-29 22:49:23 +0100

    vanbar for BoardId

---
+++ app/(platform)/(dashboard)/boards/_component/BoardNavBar.tsx
@@ -0,0 +1,18 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { board } from "@prisma/client";
+import React from "react";
+import { BoardTitleForm } from "./BoardTitleForm";
+
+interface BoardNavBarProps {
+  data: board;
+}
+async function BoardNavBar({ data }: BoardNavBarProps) {
+  return (
+    <div className="w-full h-14 text-white z-[40] x-4 md:px-10 gap-x-4 flex items-center fixed bg-black/50 top-14 ">
+      <BoardTitleForm data={data} />
+    </div>
+  );
+}
+
+export default BoardNavBar;
