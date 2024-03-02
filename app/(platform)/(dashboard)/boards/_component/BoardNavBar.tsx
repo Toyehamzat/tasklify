1a5fc78c8006a3d566cee0ffa8d3444eff2f9239
Toyehamzat <anibiseun@gmail.com>
2024-03-02 10:25:36 +0100

    added board options

--- app/(platform)/(dashboard)/boards/_component/BoardNavBar.tsx
+++ app/(platform)/(dashboard)/boards/_component/BoardNavBar.tsx
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs";
 import { board } from "@prisma/client";
 import React from "react";
 import { BoardTitleForm } from "./BoardTitleForm";
+import { BoardOptions } from "./BoardOtions";
 
 interface BoardNavBarProps {
   data: board;
@@ -11,6 +12,9 @@ async function BoardNavBar({ data }: BoardNavBarProps) {
   return (
     <div className="w-full h-14 text-white z-[40] x-4 md:px-10 gap-x-4 flex items-center fixed bg-black/50 top-14 ">
       <BoardTitleForm data={data} />
+      <div className=" ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 }
