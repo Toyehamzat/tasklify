9448a05b86f4b25c2f89ccbe3896b5feef7ac87d
Toyehamzat <anibiseun@gmail.com>
2024-03-03 01:15:35 +0100

    board id page update

--- app/(platform)/(dashboard)/boards/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/boards/[boardId]/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-function boardIdPage() {
-  return <div className="h-full">boardIdPage</div>;
+import { db } from "@/lib/db";
+
+import { ListContainer } from "./_component/listContainer";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
 }
 
-export default boardIdPage;
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
+    </div>
+  );
+};
+
+export default BoardIdPage;
