d9150be9b155727834fec7d0ef792ba278a704e6
Toyehamzat <anibiseun@gmail.com>
2024-03-03 01:16:35 +0100

    list form layout

---
+++ app/(platform)/(dashboard)/boards/[boardId]/_component/listWrapper.tsx
@@ -0,0 +1,7 @@
+interface ListWrapperProps {
+  children: React.ReactNode;
+}
+
+export const ListWrapper = ({ children }: ListWrapperProps) => {
+  return <li className="h-full shrink-0 w-[272px] select-none">{children}</li>;
+};
