b45959bb32d7a77c9bcc0bc1027552ca84ce55a1
Toyehamzat <anibiseun@gmail.com>
2024-03-05 13:37:57 +0100

    list item component

---
+++ app/(platform)/(dashboard)/boards/[boardId]/_component/listItem.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ListWithCards } from "@/types";
+import { ListWrapper } from "./listWrapper";
+import { ListHeader } from "./listHeader";
+
+interface ListItemProps {
+  id: string;
+  index: number;
+  data: ListWithCards;
+}
+export const ListItem = ({ id, index, data }: ListItemProps) => {
+  return (
+    <ListWrapper>
+      <div className="w-full rounded-md shadow-md bg-[#f1f2f4] pb-2">
+        <ListHeader data={data} />
+      </div>
+    </ListWrapper>
+  );
+};
