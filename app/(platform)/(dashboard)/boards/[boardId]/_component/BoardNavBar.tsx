9b30e4256cac9c7e431db1597acffa356b319c30
Toyehamzat <anibiseun@gmail.com>
2024-03-06 14:59:15 +0100

    changed styling in board navbar

--- app/(platform)/(dashboard)/boards/[boardId]/_component/BoardNavBar.tsx
+++ app/(platform)/(dashboard)/boards/[boardId]/_component/BoardNavBar.tsx
@@ -10,7 +10,7 @@ interface BoardNavBarProps {
 }
 async function BoardNavBar({ data }: BoardNavBarProps) {
   return (
-    <div className=" rounded-bl-xl rounded-br-xl w-full h-14 text-white z-[40] x-4 md:px-10 gap-x-4 flex items-center fixed bg-black/50 top-14 ">
+    <div className="rounded-full mt-1 w-full h-14 text-white z-[40] x-4 md:px-10 gap-x-4 flex items-center fixed bg-black/50 top-14 ">
       <BoardTitleForm data={data} />
       <div className=" ml-auto">
         <BoardOptions id={data.id} />
